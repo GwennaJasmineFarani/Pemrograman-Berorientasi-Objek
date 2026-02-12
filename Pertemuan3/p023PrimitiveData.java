f09837353258c6d88f43dd059dc10183a4de6329
Gwenna Jasmine Farani <d1041241079@student.untan.ac.id>
2026-02-12 14:09:23 +0700

    Pertemuan3

---
+++ Pertemuan3/p023PrimitiveData.java
@@ -0,0 +1,16 @@
+public class p023PrimitiveData {
+    public static void main(String[] args) {
+        int umur = 20;
+        long populasi = 8000000000L;
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+        char nilaihuruf = 'A';
+        boolean lulus = true;
+        System.out.println("umur            :" + umur);
+        System.out.println("populasi        :" + populasi);
+        System.out.println("Tinggi Badan    :" + tinggi);
+        System.out.println("IPK             :" + ipk);
+        System.out.println("Nilai Huruf     :" + nilaihuruf);
+        System.out.println("Status Lulus    :" + lulus);
+    }
+}
