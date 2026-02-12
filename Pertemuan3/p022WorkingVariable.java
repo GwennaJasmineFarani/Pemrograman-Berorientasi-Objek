f6dfb4cf7b9235e2099a6d9e0bacd1bc5c6e812e
Gwenna Jasmine Farani <d1041241079@student.untan.ac.id>
2026-02-12 14:52:58 +0700

    Pertemuan3

---
+++ Pertemuan3/p022WorkingVariable.java
@@ -0,0 +1,19 @@
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+        //Deklarasi dan inisialisasi variabel
+        int angka = 10;
+
+        //Menampilkan nilai awal variabel
+        System.out.println("Nilai awal angka: " + angka);
+
+        //Mengubah nilai variabel
+        angka = 25;
+
+        //Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: "+ angka);
+    }
+    
+}
+
+/* Nama : Gwenna Jasmine Farani
+Tanggal : 12/2/2026 */
