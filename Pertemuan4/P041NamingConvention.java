c013ecf6f0413410c4d94c70b48fe97acb921860
Gwenna Jasmine Farani <d1041241079@student.untan.ac.id>
2026-02-18 13:03:57 +0700

    Pertemuan4

---
+++ Pertemuan4/P041NamingConvention.java
@@ -0,0 +1,19 @@
+package Pertemuan4;
+
+public class P041NamingConvention {
+    public static final double PI = 3.14;
+    public static void main (String[] args) {
+        // variable dengan camelCase
+        int studentAge = 20;
+        String studentName = "Budi" ;
+        boolean isActive = true;
+
+        System.out.println("Nama:" + studentName);
+        System.out.println("Umur:" + studentAge);
+        System.out.println("Status:" + isActive);
+        System.out.println("Konstanta PI:" + PI);
+    }
+}
+
+/* Nama : Gwenna Jasmine Farani
+Tanggal : 16 Februari 2026 */
---
+++ Pertemuan4/P042OperatorAritmatika.java
@@ -0,0 +1,25 @@
+package Pertemuan4;
+
+public class P042OperatorAritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+        System.err.println();
+
+        System.out.println("a + b =" + (a + b));
+        System.out.println("a - b =" + (a - b));
+        System.out.println("a * b =" + (a * b));
+        System.out.println("a / b =" + (a / b));
+        System.out.println("a % b =" + (a % b));
+
+        // pembagian dengan desimal
+        double hasil = (double) a / b;
+        System.out.println("a / b (desimal) =" + hasil);
+    }
+}
+
+/* Nama : Gwenna Jasmine Farani
+Tanggal : 16 Februari 2026 */
