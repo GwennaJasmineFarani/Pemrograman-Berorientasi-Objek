bcd9d07a655fea85828bcb32d7b7615e79360849
Gwenna Jasmine Farani <d1041241079@student.untan.ac.id>
2026-03-08 17:00:46 +0700

    Pertemuan9

---
+++ Pertemuan9/Persegi.java
@@ -0,0 +1,42 @@
+package Pertemuan9;
+
+public class Persegi {
+
+    int sisi;
+    int luas;
+    int keliling;
+
+    // Method menghitung luas
+    public void hitungLuas() {
+        luas = sisi * sisi;
+    }
+
+    // Method menghitung keliling
+    public void hitungKeliling() {
+        keliling = 4 * sisi;
+    }
+
+    // Method menampilkan hasil
+    public void tampilkanHasil() {
+        System.out.println("=== PERSEGI ===");
+        System.out.println("Sisi     : " + sisi + " cm");
+        System.out.println("Luas     : " + luas + " cm²");
+        System.out.println("Keliling : " + keliling + " cm");
+    }
+
+    public static void main(String[] args) {
+
+        Persegi p1 = new Persegi();
+
+        // isi atribut
+        p1.sisi = 5;
+
+        // panggil method
+        p1.hitungLuas();
+        p1.hitungKeliling();
+        p1.tampilkanHasil();
+    }
+}
+
+/* Nama : Gwenna Jasmine Farani (D1041241079)
+Tanggal : 8 Maret 2026 */
